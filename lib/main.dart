4a999c0122a3d82d29d6ca93e7042b2237e21fe3
Mayank Kumar <mj112295@gmail.com>
2024-07-27 20:12:54 +0530

    Navigation bar added

--- lib/main.dart
+++ lib/main.dart
@@ -36,14 +36,24 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Center(child: Text(widget.title)),
-      ),
-      body: const CustomerBillForm(),
-    );
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Center(child: Text(widget.title)),
+        ),
+        body: const CustomerBillForm(),
+        bottomNavigationBar: NavigationBar(
+          destinations: const <NavigationDestination>[
+            NavigationDestination(
+              selectedIcon: Icon(Icons.home),
+              icon: Icon(Icons.home_outlined),
+              label: "Home",
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.people),
+              icon: Icon(Icons.people_outline),
+              label: "Customers",
+            ),
+          ],
+        ));
   }
 }
-
-
-
